ffd479bbe42a88e2c1ba8cbd363de10d3150591b
ChunKitAu <570137511@qq.com>
2020-07-19 15:39:26 +0800

    添加 canvas-nest 为登陆页面背景

--- src/views/RegLogin/RegLogin.vue
+++ src/views/RegLogin/RegLogin.vue
@@ -1,59 +1,57 @@
 <template>
-  <div class="login" :style="{backgroundImage: 'url(' + bg + ')'}">
-    <div id="card" class="login-con">
-      <div class="login-card">
-        <router-view></router-view>
-      </div>
+    <div class="login" >
+      <vueCanvasNest :config="{color:'0,0,0', count: 200}" ></vueCanvasNest>
+        <div id="card" class="login-con">
+            <div class="login-card">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      bg: require("../../assets/bg.png"),
+    import vueCanvasNest from 'vue-canvas-nest'
+    export default {
+        components: {
+            vueCanvasNest
+        },
     };
-  },
-  mounted() {
-  }
-};
 </script>
 
 <style scoped>
-.login {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-}
+    .login {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+    }
 
-.login-con {
-  width: 410px;
-  min-width: 410px;
-  padding: 10px;
-  background-color: #fff;
-  border-color: transparent;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.05);
-  position: relative;
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-direction: column;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border-radius: 0.8rem;
-}
+    .login-con {
+        width: 410px;
+        min-width: 410px;
+        padding: 10px;
+        background-color: #fff;
+        border-color: transparent;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.05);
+        position: relative;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        min-width: 0;
+        word-wrap: break-word;
+        background-color: #fff;
+        background-clip: border-box;
+        border-radius: 0.8rem;
+    }
 
-.login-card {
-  border: none;
-  flex: 1 1 auto;
-  padding: 1.25rem;
-}
+    .login-card {
+        border: none;
+        flex: 1 1 auto;
+        padding: 1.25rem;
+    }
 </style>
